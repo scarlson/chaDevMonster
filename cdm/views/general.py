486d3fbb40ae499815a8909d895e2b992e15a0e4
juzten <juzten@gmail.com>
2015-07-14 07:36:17 +0100

    adding git hook for auto deployment

--- cdm/views/general.py
+++ cdm/views/general.py
@@ -2,6 +2,9 @@
 
 import os
 import arrow
+import hmac
+import hashlib
+import subprocess
 from flask import Blueprint, render_template, flash
 from flask import Flask, request
 from cdm.forms.forms import RedditPostForm
@@ -67,6 +70,35 @@ def reddit_callback():
     code = request.args.get('code')
     # We'll change this next line in just a moment
     return "got a code! %s" % code
+
+@mod.route("/payload", methods=['POST'])
+def github_payload():
+    signature = request.headers.get('X-Hub-Signature')
+    data = request.data
+    if verify_hmac_hash(data, signature):
+        if request.headers.get('X-GitHub-Event') == "ping":
+            return jsonify({'msg': 'Ok'})
+        if request.headers.get('X-GitHub-Event') == "push":
+            payload = request.get_json()
+            if payload['commits'][0]['distinct'] == True:
+                try:
+                    cmd_output = subprocess.check_output(
+                        ['git', 'pull', 'origin', 'master'],)
+                    cmd_output = cmd_output + subprocess.check_output(
+                        ['sudo', 'supervisorctl', 'restart', 'gunicorn'],)
+
+                    return jsonify({'msg': str(cmd_output)})
+                except subprocess.CalledProcessError as error:
+                    return jsonify({'msg': str(error.output)})
+
+    else:
+        return jsonify({'msg': 'invalid hash'})
+
+
+def verify_hmac_hash(data, signature):
+    github_secret = config.GITHUB_SECRET
+    mac = hmac.new(github_secret, msg=data, digestmod=hashlib.sha1)
+    return ('sha1='+mac.hexdigest()) == signature
 
 
 def link_in_db(link):
